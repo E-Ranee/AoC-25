76c7f9aabb1056b9de9d7b7ae5e19ca3c790c82c
Esina Ranee <esina.ranee@gmail.com>
2025-12-03 11:24:46 +0000

    Cleaned up a little

--- day 3/python.py
+++ day 3/python.py
@@ -12,44 +12,17 @@ data = []
 for row in file_data:
     data.append([int(x) for x in row.strip().split()[0]])
 
-joltages = []
-for battery_bank in data:
-    first_digit = max(battery_bank)
-    index = battery_bank.index(first_digit)
-    if index == len(battery_bank) - 1: # if max is rightmost digit
-        second_digit = first_digit
-        first_digit = max(battery_bank[:-1])
-    else:
-        second_digit = max(battery_bank[index+1:])
-    joltage = (first_digit * 10) + second_digit # concatonate strings
-    joltages.append(joltage)
-
-# print(sum(joltages))
-
-
-# part A
-# find highest digit
-# find index
-# find highest digit to the right
-
-# what if highest digit is the right-most?
-
-# part B: what is the highest digit [x+] digits from the right
-# what is the next highest digits [x+:x-1] digits from the right
-
 def get_joltage(battery_bank, number_of_batteries):
     """Works by looking at the section of the list after the digits already found and before {number of batteries remaining} from the end"""
     digits = []
     min_index = 0
-    current_index = 0
     for i in range(number_of_batteries): # loop this many times
         unreserved = len(battery_bank) - number_of_batteries + len(digits) + 1 # keep the last x digits free for future loops
         current_slice = battery_bank[min_index:unreserved]
         current_digit = max(current_slice) # the highest number in the available slice
-        while battery_bank[current_index] != current_digit: # increment index until you reach the position of this number
-            current_index += 1
-        min_index = current_index + 1
-        current_index += 1 
+        while battery_bank[min_index] != current_digit: # increment index until you reach the position of this number
+            min_index += 1
+        min_index += 1 
         digits.append(current_digit)
 
     string = ""
